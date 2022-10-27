de8f13a008bc09e15efc6117f91ddd0f86d87fc3
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-27 14:43:02 -0300

    Add types

--- typescript/fetchJson/index.js
+++ typescript/fetchJson/index.js
@@ -2,5 +2,12 @@
 exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios_1["default"].get(url)
-    .then(function (response) { return console.log(response.data); })["catch"](function (error) { return console.log(error); });
+axios_1["default"]
+    .get(url)
+    .then(function (response) {
+    var toDo = response.data;
+    var id = toDo.id;
+    var title = toDo.title;
+    var finished = toDo.finished;
+    console.log("\n      The Todo with ID: ".concat(id, " \n      Has a title of: ").concat(title, " \n      Is it finished?: ").concat(finished, "\n      "));
+})["catch"](function (error) { return console.log(error); });
--- typescript/fetchJson/index.ts
+++ typescript/fetchJson/index.ts
@@ -2,6 +2,28 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url)
-  .then(response => console.log(response.data))
-  .catch(error => console.log(error))
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+axios
+	.get(url)
+	.then((response) => {
+		const toDo = response.data as Todo;
+
+		const id = toDo.id;
+		const title = toDo.title;
+		const completed = toDo.completed;
+		logTodo(id, title, completed);
+	})
+	// .catch((error) => console.log(error));
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+	console.log(`
+      The Todo with ID: ${id} 
+      Has a title of: ${title} 
+      Is it finished?: ${completed}
+      `);
+};
