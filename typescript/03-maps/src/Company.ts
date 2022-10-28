102d458f10f779e40bf52ac14ad4e4cc1e48863d
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-28 17:40:08 -0300

    Finish app

--- typescript/03-maps/src/Company.ts
+++ typescript/03-maps/src/Company.ts
@@ -1,19 +1,27 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class Company {
-  companyName: string;
-  catchPhrase: string;
-  location: {
-    lat: number,
-    lng: number
-  }
+export class Company implements Mappable{
+	companyName: string;
+	catchPhrase: string;
+	location: {
+		lat: number;
+		lng: number;
+	};
 
-  constructor() {
-    this.companyName = faker.company.companyName()
-    this.catchPhrase = faker.company.catchPhrase()
-    this.location = {
-      lat: parseFloat(faker.address.latitude()),
-      lng: parseFloat(faker.address.longitude())
-    }
-  }
+	constructor() {
+		this.companyName = faker.company.companyName();
+		this.catchPhrase = faker.company.catchPhrase();
+		this.location = {
+			lat: parseFloat(faker.address.latitude()),
+			lng: parseFloat(faker.address.longitude()),
+		};
+	}
+
+	markerContent(): string {
+		return `<div>
+              <h3>User Name: ${this.companyName}</h3>
+              <p>Catchphrase: ${this.catchPhrase}</p>
+            </div>`;
+	}
 }
--- typescript/03-maps/src/CustomMap.ts
+++ typescript/03-maps/src/CustomMap.ts
@@ -1,29 +1,47 @@
 /// <reference types="@types/google.maps" />
-import { User } from './User'
-import { Company } from './Company'
+
+// Instructions to every other class
+// on how the can be an argument to 'addMarker'
+export interface Mappable {
+	location: {
+		lat: number;
+		lng: number;
+	};
+	markerContent(): string;
+}
 
 export class CustomMap {
-  private googleMap: google.maps.Map
+	private googleMap: google.maps.Map;
 
-  constructor(divId: string) {
-    this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-      zoom: 1,
-      center: {
-        lat: 0,
-        lng: 0
-      }
-    })
-  }
+	constructor(divId: string) {
+		this.googleMap = new google.maps.Map(
+			document.getElementById(divId) as HTMLElement,
+			{
+				zoom: 1,
+				center: {
+					lat: 0,
+					lng: 0,
+				},
+			}
+		);
+	}
 
-  addMarker(mappable: User | Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: mappable.location.lat,
-        lng: mappable.location.lng
-      }
-    })
-  }
+	addMarker(mappable: Mappable): void {
+		const marker = new google.maps.Marker({
+			map: this.googleMap,
+			position: {
+				lat: mappable.location.lat,
+				lng: mappable.location.lng,
+			},
+		});
 
+		marker.addListener('click', () => {
+			const infoWindow = new google.maps.InfoWindow({
+				content: mappable.markerContent(),
+			});
 
+			infoWindow.open(this.googleMap, marker);
+		});
+	}
+  
 }
