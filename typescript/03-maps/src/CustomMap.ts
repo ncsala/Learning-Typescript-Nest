c4b6bd96b0b0bf2fafe0d3db5ae35e792fedb766
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-28 16:49:26 -0300

    Update .gitignore

--- typescript/03-maps/src/CustomMap.ts
+++ typescript/03-maps/src/CustomMap.ts
@@ -1,4 +1,5 @@
-
+import { User } from './User'
+import { Company } from './Company'
 
 export class CustomMap {
   private googleMap: google.maps.Map
@@ -13,4 +14,24 @@ export class CustomMap {
     })
   }
 
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+    })
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng
+      }
+    })
+
+  }
 }
