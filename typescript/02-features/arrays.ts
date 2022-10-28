c9698a873020c968b6f990617ffe2cfde6764b25
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-28 13:25:15 -0300

    Change in arrays

--- typescript/02-features/arrays.ts
+++ typescript/02-features/arrays.ts
@@ -9,7 +9,7 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // Prevent incompatible values
-carMakers.push(100);
+// carMakers.push(100);
 
 // Help with 'map', 'reduce', 'forEach', etc
 // Como sabe que lo que recorrera sera una string, cuando escribis car.
@@ -22,4 +22,4 @@ carMakers.map((car: string): string => {
 const importantDates: (string | Date)[] = [new Date(), '2030-10-10'];
 importantDates.push('2030-10-15');
 importantDates.push(new Date());
-importantDates.push(15);
+// importantDates.push(15);
