f7c8c978ca68523cfe911c1839d593199e1e67e7
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-29 23:19:07 -0300

    Conection with db, create entities

--- nest-projects/car-value/src/app.module.ts
+++ nest-projects/car-value/src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { ReportsController } from './reports/reports.controller';
-import { UsersService } from './users/users.service';
-import { ReportsService } from './reports/reports.service';
 import { UsersModule } from './users/users.module';
-import { ReportrsModule } from './reportrs/reportrs.module';
 import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity'
+import { Report } from './reports/reports.entity' 
 
 @Module({
-  imports: [UsersModule, ReportrsModule, ReportsModule],
-  controllers: [AppController, UsersController, ReportsController],
-  providers: [AppService, UsersService, ReportsService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Report],
+    synchronize: true,
+  }), UsersModule, ReportsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- nest-projects/car-value/src/users/users.module.ts
+++ nest-projects/car-value/src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.entity'
 
-@Module({})
+@Module({
+  // Este paso crea el repository por nosotros.
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService]
+})
 export class UsersModule {}
