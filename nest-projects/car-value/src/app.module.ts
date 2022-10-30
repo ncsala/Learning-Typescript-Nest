60847d71cfc4a94ba37877b37a51a42ea202db6f
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-29 22:28:49 -0300

    Create modules reports and users

--- nest-projects/car-value/src/app.module.ts
+++ nest-projects/car-value/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { ReportsController } from './reports/reports.controller';
+import { UsersService } from './users/users.service';
+import { ReportsService } from './reports/reports.service';
+import { UsersModule } from './users/users.module';
+import { ReportrsModule } from './reportrs/reportrs.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, ReportrsModule, ReportsModule],
+  controllers: [AppController, UsersController, ReportsController],
+  providers: [AppService, UsersService, ReportsService],
 })
 export class AppModule {}
