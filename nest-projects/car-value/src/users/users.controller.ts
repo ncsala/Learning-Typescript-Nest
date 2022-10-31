0e5b6cffa0d365d0a9f0b9004a9eb7326b0b6730
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-31 00:53:32 -0300

    Create interceptors

--- nest-projects/car-value/src/users/users.controller.ts
+++ nest-projects/car-value/src/users/users.controller.ts
@@ -12,8 +12,11 @@ import {
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -22,8 +25,10 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  // @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('handler is running')
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('User not found');
