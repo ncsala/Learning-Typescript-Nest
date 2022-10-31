4bdffddba04dc01c92f569f7f051742ef5e8adb7
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-30 22:32:18 -0300

    Working on Users Module

--- nest-projects/car-value/src/users/users.controller.ts
+++ nest-projects/car-value/src/users/users.controller.ts
@@ -1,13 +1,53 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body)
+    this.usersService.create(body.email, body.password);
   }
 
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
 
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  // @Get('/list')
+  // listUsers() {
+  //   return this.usersService.findAll()
+  // }
 }
--- nest-projects/car-value/src/users/users.service.ts
+++ nest-projects/car-value/src/users/users.service.ts
@@ -1,4 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // repo: Repository<User>
+
+  // constructor(repo: Repository<User>) {
+  //   this.repo = repo
+  // }
+
+  // Recordar que esto es lo mismo que las lineas de arriba
+  // Repo sera una instancia de typeorm Repository que lidia con la instancia usuario.
+  // O sea ese repositorio manejara las instancias usuario.
+  // El InjectRepository se pone pq el sistema de injeccion de dependencia
+  // no trabaja bien con "tipos genericos", por eso usamos el decorador
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // Crea una nueva instancia de usuario con los datos
+    // pasados a traves de la request
+    const user = this.repo.create({ email, password });
+
+    // Persiste esa entidad usuario en la db
+    return this.repo.save(user);
+  }
+
+  findAll() {
+    const userList = this.repo.find();
+    return userList;
+  }
+
+  findOne(id: number) {
+    const user = this.repo.findOneBy({ id: id });
+
+    return user;
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  // "Partial" le dice que al menos debe recibir un atritubot de la
+  // clase (en este caso User)
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    // Object.assign(user, attrs);
+    // alternativa a object.assign
+    const userUpdated = { ...user, ...attrs };
+
+    return this.repo.save(userUpdated);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repo.remove(user);
+  }
+}
