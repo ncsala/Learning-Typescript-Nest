8a61747467ab226259cbee60bbd3adaf7cce4d45
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-10-31 23:51:44 -0300

    Configure .env and update .gitignore

--- nest-projects/car-value/src/app.module.ts
+++ nest-projects/car-value/src/app.module.ts
@@ -1,19 +1,45 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
+// ConfigModule: para configurar cual de los archivos .evn se usara
+// ConfigService: expone la informacion de los env al resto de la aplicacion
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity'
-import { Report } from './reports/reports.entity' 
+import { User } from './users/user.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true,
-  }), UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      // Para no tener que configurar en cada modulo, se pone global
+      isGlobal: true,
+      // Se lee en testing o development segun el
+      // ambiente q este corriendo
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        }
+      }
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
