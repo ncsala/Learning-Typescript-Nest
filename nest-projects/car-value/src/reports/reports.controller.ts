7e88092d0adffbd79e91ee4cc0f8e218e16f84a9
Nicolas Martin Caceres Sala <martin.sala@cencosud.com>
2022-11-01 02:11:39 -0300

    Create route POST /reports

--- nest-projects/car-value/src/reports/reports.controller.ts
+++ nest-projects/car-value/src/reports/reports.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    console.log(user)
+    return this.reportsService.create(body, user);
+  }
+}
--- nest-projects/car-value/src/reports/reports.entity.ts
+++ nest-projects/car-value/src/reports/reports.entity.ts
@@ -1,26 +1,30 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { User } from 'src/users/user.entity';
 
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  price: number;
-
   // Toyota, Hyundai, Ford, etc
   @Column()
-  make: number
+  make: string;
 
   @Column()
   year: number;
 
   @Column()
-  lng: number
+  lng: number;
 
   @Column()
-  lat: number
+  lat: number;
 
   @Column()
-  mileage: number
+  mileage: number;
+
+  @Column()
+  price: number;
+
+  @ManyToOne(() => User, (user) => user.reports)
+  user: User;
 }
